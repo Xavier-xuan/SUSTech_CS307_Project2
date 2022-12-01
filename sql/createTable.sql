9b9d4f104a6267bba09c1d3d4b7f98adf05e159d
seth <22097951+Seth2482@users.noreply.github.com>
2022-12-01 15:56:47 +0800

    modified the structure of tables, and add sqls of create users.

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -1,3 +1,6 @@
+CREATE SCHEMA IF NOT EXISTS project2;
+SET SCHEMA 'project2';
+
 CREATE TABLE port_city
 (
     name varchar(50) primary key
@@ -19,7 +22,7 @@ CREATE TABLE company
     name varchar(50) primary key
 );
 
-CREATE TABLE manager
+CREATE TABLE company_manager
 (
     name         varchar(50) primary key,
     phone_number varchar(50),
@@ -27,6 +30,15 @@ CREATE TABLE manager
     age          int,
     password     text,
     company_name varchar(50) references company (name)
+);
+
+CREATE TABLE department_manager
+(
+    name         varchar(50) primary key,
+    phone_number varchar(50),
+    gender       varchar(10),
+    age          int,
+    password     text
 );
 
 CREATE TABLE courier
@@ -58,32 +70,21 @@ CREATE TABLE ship
 
 CREATE TABLE item
 (
-    name           varchar(50) primary key,
-    price          numeric(10, 2),
-    type           varchar(50),
-    export_tax     numeric(10, 2),
-    import_tax     numeric(10, 2),
-    from_city_name varchar(50) references city (name),
-    to_city_name   varchar(50) references city (name),
-    container_code varchar(50) references container (code),
-    ship_name      varchar(50) references ship (name),
-    state          varchar(50)
+    name              varchar(50) primary key,
+    price             numeric(10, 2),
+    type              varchar(50),
+    export_tax        numeric(10, 2),
+    import_tax        numeric(10, 2),
+    export_city       varchar(50) references port_city,
+    import_city       varchar(50) references port_city,
+    export_officer    varchar(50) references officer (name),
+    import_officer    varchar(50) references officer (name),
+    from_city_name    varchar(50) references city (name),
+    to_city_name      varchar(50) references city (name),
+    delivery_courier  varchar(50) references courier (name),
+    retrieval_courier varchar(50) references courier (name),
+    container_code    varchar(50) references container (code),
+    ship_name         varchar(50) references ship (name),
+    state             varchar(50)
 );
 
-CREATE TABLE delivery_and_retrieval
-(
-    id           SERIAL primary key,
-    item_name    varchar(50) references item (name),
-    type         varchar(50) not null,
-    city_name    varchar(50) not null references city (name),
-    courier_name varchar(50) not null references courier (name)
-);
-
-CREATE TABLE export_and_import
-(
-    id             SERIAL primary key,
-    type           varchar(50) not null,
-    item_name      varchar(50) references item (name),
-    officer_name   varchar(50) references officer (name),
-    port_city_name varchar(50) references port_city (name)
-)
---
+++ sql/createUser.sql
@@ -0,0 +1,12 @@
+SET SCHEMA 'project2';
+
+CREATE USER courier WITH PASSWORD 'courier_password';
+CREATE USER officer WITH PASSWORD 'officer_password';
+CREATE USER company_manager WITH PASSWORD 'company_manager_password';
+CREATE USER department_manager WITH PASSWORD 'department_manager_password';
+
+GRANT SELECT,UPDATE ON TABLE item,courier TO courier;
+GRANT SELECT,UPDATE ON TABLE item,officer TO officer;
+GRANT SELECT ON TABLE ship TO company_manager;
+GRANT SELECT,UPDATE ON TABLE item,company TO company_manager;
+GRANT SELECT ON ALL TABLES IN SCHEMA project2 TO department_manager
