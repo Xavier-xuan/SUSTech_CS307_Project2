34da8735b8854e93423b25a4e43f87e4d142a368
Mark4551124015 <mark4551124015@gmail.com>
2022-12-03 22:19:26 +0800

    new lib file

--- src/DatabaseManipulation.java
+++ src/DatabaseManipulation.java
@@ -1,103 +1,11 @@
-import cs307.project2.interfaces.*;
+import cs307.project2.interfaces.IDatabaseManipulation;
 
 public class DatabaseManipulation implements IDatabaseManipulation {
+
+
     @Override
     public void $import(String s, String s1) {
 
     }
 
-    @Override
-    public double getImportTaxRate(LogInfo logInfo, String s, String s1) {
-        return 0;
-    }
-
-    @Override
-    public double getExportTaxRate(LogInfo logInfo, String s, String s1) {
-        return 0;
-    }
-
-    @Override
-    public boolean loadItemToContainer(LogInfo logInfo, String s, String s1) {
-        return false;
-    }
-
-    @Override
-    public boolean loadContainerToShip(LogInfo logInfo, String s, String s1) {
-        return false;
-    }
-
-    @Override
-    public boolean shipStartSailing(LogInfo logInfo, String s) {
-        return false;
-    }
-
-    @Override
-    public boolean unloadItem(LogInfo logInfo, String s) {
-        return false;
-    }
-
-    @Override
-    public boolean itemWaitForChecking(LogInfo logInfo, String s) {
-        return false;
-    }
-
-    @Override
-    public boolean newItem(LogInfo logInfo, ItemInfo itemInfo) {
-        return false;
-    }
-
-    @Override
-    public boolean setItemState(LogInfo logInfo, String s, ItemState itemState) {
-        return false;
-    }
-
-    @Override
-    public String[] getAllItemsAtPort(LogInfo logInfo) {
-        return new String[0];
-    }
-
-    @Override
-    public boolean setItemCheckState(LogInfo logInfo, String s, boolean b) {
-        return false;
-    }
-
-    @Override
-    public int getCompanyCount(LogInfo logInfo) {
-        return 0;
-    }
-
-    @Override
-    public int getCityCount(LogInfo logInfo) {
-        return 0;
-    }
-
-    @Override
-    public int getCourierCount(LogInfo logInfo) {
-        return 0;
-    }
-
-    @Override
-    public int getShipCount(LogInfo logInfo) {
-        return 0;
-    }
-
-    @Override
-    public ItemInfo getItemInfo(LogInfo logInfo, String s) {
-        return null;
-    }
-
-    @Override
-    public ShipInfo getShipInfo(LogInfo logInfo, String s) {
-        return null;
-    }
-
-    @Override
-    public ContainerInfo getContainerInfo(LogInfo logInfo, String s) {
-        return null;
-    }
-
-    @Override
-    public StaffInfo getStaffInfo(LogInfo logInfo, String s) {
-        return null;
-    }
 }
