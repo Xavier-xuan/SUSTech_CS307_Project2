22457d0b55f0b0115d44dad21185c91fa0b68cef
seth <22097951+Seth2482@users.noreply.github.com>
2022-12-18 12:16:28 +0800

    finish login example

--- CS307_Project_FrontEnd/login.py
+++ CS307_Project_FrontEnd/login.py
@@ -1,4 +1,10 @@
 import util as u
+from getpass import getpass
+import requests
+from config import config
+from urllib.parse import urljoin
+
+
 
 def login() :
 
@@ -12,20 +18,20 @@ def login() :
         print("|      Please Enter Numer To Select Your Role First        |")
         print("|----------------------------------------------------------|")
         print("|  1.Courier       2.Company Manager    3.Seaport Officer  |")
-        print("|  4.SUSTCManger                        X.Exit             |")
+        print("|  4.SUSTCManager                       X.Exit             |")
         print("============================================================")
         if info != "":
             print(info)
         print(">>> ",end="")
         op = input();
         if op == '1':
-            role = "Courier"
+            role = "courier"
         elif op =='2':
-            role = "Company Manager"
+            role = "company manager"
         elif op == '3':
-            role = "Seaport Officer"
+            role = "seaport officer"
         elif op == '4':
-            role = "SUSTCManger"
+            role = "sustc manager"
         else:
             info = ("<Wrong Selection>")
             flag = True
@@ -35,9 +41,23 @@ def login() :
     print("|        Please Enter Your Username and Password           |")
     print("============================================================")
     print(">>> Userbame: ", end="")
-    username = input();
-    print(">>> Password: ", end="")
-    passwd = input();
+    username = input()
+    passwd = getpass(prompt=">>> Password: ")
+
+    r = requests.post(urljoin(config['base'], '/login'), data = {
+        "username": username,
+        "role": role,
+        "password": passwd
+    })
+    result = r.json()
+    if(result):
+        print("Login Successfully!")
+    else:
+        print("Wrong Username or Password!")
+
+if __name__ == '__main__':
+    login()
+
 
 
 
