09d28afc721e3390a5d82f5ce273c935f26a90bf
seth <22097951+Seth2482@users.noreply.github.com>
2022-12-23 13:58:34 +0800

    delete useless file

--- old_structure_project/cs307.project2.interfaces/src/cs307/project2/interfaces/IDatabaseManipulation.java
+++
@@ -1,9 +0,0 @@
-package cs307.project2.interfaces;
-
-public interface IDatabaseManipulation {
-	// Your implemented class shall have constructor like:
-	//		Constructor(String database, String root, String pass)
-	// See project description for more details.
-
-	void $import(String recordsCSV, String staffsCSV);
-}
--- old_structure_project/cs307.project2.interfaces/src/cs307/project2/interfaces/ISeaportOfficer.java
+++
@@ -1,7 +0,0 @@
-package cs307.project2.interfaces;
-
-public interface ISeaportOfficer {
-	String[] getAllItemsAtPort(LogInfo log);
-	
-	boolean setItemCheckState(LogInfo log, String itemName, boolean success);
-}
